46e3bb251a4439c64dbfe0658db0f9cd396ad89a
chris-skud <chris.skud@gmail.com>
2016-01-21 17:30:28 -0700

    messing around with a possible strava shaming bot

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+"use strict";
+
 var Botkit = require('botkit')
+var strava = require('strava-v3')
 
 // Expect a SLACK_TOKEN environment variable
 var slackToken = process.env.SLACK_TOKEN
 if (!slackToken) {
   console.error('SLACK_TOKEN is required!')
+  process.exit(1)
+}
+
+var clubId = process.env.CLUB_ID
+if (!clubId) {
+  console.error('CLUB_ID is required!')
   process.exit(1)
 }
 
@@ -22,47 +31,49 @@ controller.on('bot_channel_join', function (bot, message) {
   bot.reply(message, "I'm here!")
 })
 
-controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
-  bot.reply(message, 'Hello.')
-})
+var motivations = [
+  'Train insane or remain the same.',
+  'You look like a can of biscuits that’s just popped open.',
+  'Your tears are like jet fuel to me.',
+  'Gat yer arse on that tarmac ya Big Mac eating duffelbagers.',
+  'All I do is eat gunpowder and run. What about you?',
+  'Go away, you smell like failure and corn chips.',
+  'There will be no crying or whining for any reason or a reason will be issued to you.',
+]
 
-controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
-  bot.reply(message, 'Hello.')
-  bot.reply(message, 'It\'s nice to talk to you directly.')
-})
+controller.hears('lazy', ['direct_message','direct_mention'], function (bot, message) {
+  strava.clubs.listMembers({id:clubId},function(err, members) {
+    strava.clubs.listActivities({id:clubId},function(err, activities) {
+      let shamingList = []
+      members.forEach(function(member) {
+        let memberHasActivity = false
+        activities.forEach(function(activity) {
+          if (member.id === activity.athlete.id) {
+            memberHasActivity = true
+          }
+        });
 
-controller.hears('.*', ['mention'], function (bot, message) {
-  bot.reply(message, 'You really do care about me. :heart:')
+        if (!memberHasActivity) {
+          shamingList.push(member)
+          console.log(member.firstname + " does not have activities")
+        }
+      });
+
+      let lazyStr = ""
+      shamingList.forEach(function(shamee, index) {
+        let comma = (index !== shamingList.length) ? ", " : ""
+        lazyStr += shamee.firstname + " " + shamee.lastname + comma
+      });
+
+      lazyStr += motivations[Math.floor(Math.random()*(motivations.length+1))]
+      bot.reply(message, lazyStr)
+    });
+  });
 })
 
 controller.hears('help', ['direct_message', 'direct_mention'], function (bot, message) {
   var help = 'I will respond to the following messages: \n' +
-      '`bot hi` for a simple message.\n' +
-      '`bot attachment` to see a Slack attachment message.\n' +
-      '`@<your bot\'s name>` to demonstrate detecting a mention.\n' +
+      '`strava leaderboard` for a simple message.\n' +
       '`bot help` to see this again.'
   bot.reply(message, help)
-})
-
-controller.hears(['attachment'], ['direct_message', 'direct_mention'], function (bot, message) {
-  var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
-  var attachments = [{
-    fallback: text,
-    pretext: 'We bring bots to life. :sunglasses: :thumbsup:',
-    title: 'Host, deploy and share your bot in seconds.',
-    image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
-    title_link: 'https://beepboophq.com/',
-    text: text,
-    color: '#7CD197'
-  }]
-
-  bot.reply(message, {
-    attachments: attachments
-  }, function (err, resp) {
-    console.log(err, resp)
-  })
-})
-
-controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
-  bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
 })
